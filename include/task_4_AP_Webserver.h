aa728d72888bbef740fd072270c886beb6d03ec4
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2026-04-14 21:53:43 +0700

    hieu webserver

--- include/task_4_AP_Webserver.h
+++ include/task_4_AP_Webserver.h
@@ -1,18 +1,23 @@
 #ifndef TASK_4_AP_WEBSERVER_H
 #define TASK_4_AP_WEBSERVER_H
+
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <ArduinoJson.h> //JSON
+#include <ArduinoJson.h>
 #include <LittleFS.h>
-#include <Update.h> //OTA
+#include <Update.h>
 #include <PubSubClient.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define LED_PIN GPIO_NUM_5
+#define WIFI_CONNECTED_NOTIFY_BIT (1 << 0)
 
-extern const char* ssid;
-extern const char* password;
 extern AsyncWebServer server;
+
+// Handle của task LED
+extern TaskHandle_t ledTaskHandle;
 
 void mountFlash(void *pvParameters);
 void settingsWifi(void *pvParameters);
@@ -20,5 +25,6 @@ void webServerTask(void *pvParameters);
 void webBackend(void *pvParameters);
 void mqttCallback(char *topic, byte *payload, unsigned int length);
 void handleMQTT();
+void handleWiFiNotify();
 
-#endif // TASK_4_AP_WEBSERVER_H
+#endif
